d273c33bcf67b97196d82451c04ffade342156f8
dataricardom <dataricardom@gmail.com>
2025-11-06 04:23:16 +0000

    feat convert to fsql file

---
+++ 00.config/configuracoes.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+CREATE DATABASE IF NOT EXISTS kpuudata.bronze;
+
+-- COMMAND ----------
+
+CREATE DATABASE IF NOT EXISTS kpuudata.silver;
+
+-- COMMAND ----------
+
+CREATE DATABASE IF NOT EXISTS kpuudata.gold;
